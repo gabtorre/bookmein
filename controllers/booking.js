cba2a3b55fa5c02f48f337c96df0d5a80dd5425c
binay bishwakarma <khadkabinay@yahoo.com>
2020-09-19 14:07:06 -0500

    adds index route in booking.js

---
+++ controllers/booking.js
@@ -0,0 +1,24 @@
+
+/* External mudules */
+const express = require('express')
+const router = express.Router()
+const db  = require('../models')
+
+
+
+
+// Index Route
+router.get('/', (req, res)=>{
+    
+    db.Booking.find({}, (error , foundbookings)=>{
+        if(error){
+            return res.send(error)
+        }else{
+          const context = {bookings: foundbookings}
+           
+          res.render('./booking/index.ejs', context)
+         
+  
+        }
+    } )  
+  })
