0226fa2784876ad4bed861d90f924c3eb94fad76
binay bishwakarma <khadkabinay@yahoo.com>
2020-09-19 16:35:25 -0500

    adds post route in booking.js

--- controllers/booking.js
+++ controllers/booking.js
@@ -31,4 +31,29 @@ router.get('/new', (req, res)=>{
  
   })
 
+
+  // Post Route
+router.post('/' , (req, res)=>{
+    
+    // defines an array of days
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+ // takes a date and asigns it 
+let day = new Date(req.body.day)
+
+// asigns a day to req.body.day 
+req.body.day = days[day.getDay()]
+
+db.Booking.create(req.body, (error, createdADay)=>{ 
+        if(error){
+            return res.send(error)
+        }else{
+            res.redirect('/booking')
+        }
+    })
+})
+
+
+
+
   module.exports = router
