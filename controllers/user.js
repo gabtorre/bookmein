2a0d7d39e7bc7f17e6fd234359d7e73ff0e93d71
gabtorre <gtorresx3@gmail.com>
2020-09-24 18:56:35 -0700

    Updates Front End

--- controllers/user.js
+++ controllers/user.js
@@ -102,7 +102,7 @@ router.put('/:id', (req, res) => {
         if (error) {
             console.log(error)
         } else {
-            res.redirect(`/user/${updatedUser._id}`)
+            res.redirect(`/user/${updatedUser._id}/account`)
         }
     })
 });
@@ -115,7 +115,7 @@ router.delete('/:id', (req, res) => {
         if (error) {
             console.log(error)
         } else {
-            res.redirect('/user')
+            res.redirect('/')
         }
     })
 });
